7bef7b29df8d13b2547d07994d94113b3e7397ef
Haley <66721019+HaleyJe0ng@users.noreply.github.com>
2022-02-23 20:32:10 +0900

    added fill/Eraser/Save mode

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,17 +3,27 @@ const ctx = jsCanvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
-
+const remove = document.getElementById("jsRemove");
+const saveBtn = document.getElementById("jsSave");
+const eraser = document.getElementById("jsEraser");
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 let painting = false;
 let filling = false;
-
+let erasing = false;
 /*canvas는 css 사이즈와 pixel 사이즈를 가져야함 
     pixel manipulation */
-jsCanvas.width = 700;
-jsCanvas.height = 700;
-ctx.strokeStyle = "#2c2c2c";
+jsCanvas.width = CANVAS_SIZE;
+jsCanvas.height = CANVAS_SIZE;
+/*added background*/
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, jsCanvas.width, jsCanvas.height);
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = "2.5";
-ctx.fillRect(50, 20, 100, 49);
+//ctx.fillStyle = "green";
+//ctx.fillRect(50, 20, 100, 49);
 
 function stopPainting() {
   painting = false;
@@ -46,6 +56,7 @@ clientX,Y: 윈도우 전체의 좌표를 가져옴 */
 function handleColorClick(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
@@ -57,20 +68,57 @@ function handleRangeChange(event) {
 function handleModeClick() {
   if (filling === true) {
     filling = false;
+    erasing = false;
     mode.innerText = "Fill";
   } else {
     filling = true;
+    erasing = false;
     mode.innerText = "Paint";
   }
 }
 
-function fillCanvas() {}
+function handleCanvasClick() {
+  if (!erasing) {
+    if (filling) {
+      ctx.fillRect(0, 0, jsCanvas.width, jsCanvas.height);
+    }
+  }
+}
+
+function handleRemoveClick() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, jsCanvas.width, jsCanvas.height);
+}
+
+function handleCM(event) {
+  event.preventDefault();
+  //prevent right mouse click;
+}
+
+function handleSaveClick() {
+  const image = jsCanvas.toDataURL("image/png");
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS";
+  link.click(); //fake click을 만들어줌
+  //console.log(link);
+}
+
+function handleEraserClick() {
+  erasing = true;
+  if (filling === true) {
+    filling = false;
+  }
+  ctx.strokeStyle = "white";
+}
 
 if (jsCanvas) {
   jsCanvas.addEventListener("mousemove", onMouseMove);
   jsCanvas.addEventListener("mousedown", startPainting);
   jsCanvas.addEventListener("mouseup", stopPainting);
   jsCanvas.addEventListener("mouseleave", stopPainting);
+  jsCanvas.addEventListener("click", handleCanvasClick);
+  jsCanvas.addEventListener("contextmenu", handleCM);
 }
 
 //Array.from() object로부터 array를 생성한다
@@ -84,4 +132,16 @@ if (range) {
 
 if (mode) {
   mode.addEventListener("click", handleModeClick);
+}
+
+if (remove) {
+  remove.addEventListener("click", handleRemoveClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
+}
+
+if (eraser) {
+  eraser.addEventListener("click", handleEraserClick);
 }
